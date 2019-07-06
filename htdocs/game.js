851b255ca528786ac1f0723215fc989dcbf2d58b
unobatbayar <uno02bt@gmail.com>
2019-07-06 20:57:32 +0100

    fix: removed file

--- htdocs/game.js
+++
@@ -1,5 +0,0 @@
-function Contact() {
-  var members = [" Steven Lua", " Larisa Budaeva", " Uno Batbayar", " Leare Song", " Bach Adylbekov"]; 
-  var icon = '<span uk-icon="grid"></span>';
-  return icon + members;
-}
